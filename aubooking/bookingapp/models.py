06ae93a765b8eae6f40f7e7ede647dc9b6e9fd80
arlen <arlenismailov02@gmail.com>
2024-05-02 19:38:50 +0600

    models

--- aubooking/bookingapp/models.py
+++ aubooking/bookingapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Hotel(models.Model):
+    name = models.CharField(40)
+    description = models.TextField()
+    address = models.CharField(40)
+    city = models.CharField(30)
+    country = models.CharField(30)
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_date = models.DateTimeField
+    parent = models.
+    hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE)
+    rating = models.
+
+
